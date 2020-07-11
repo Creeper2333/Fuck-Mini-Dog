a3a5368fcbdebabab04f21a89568f9bcacb14856
Creeper233 <52315359+Creeper2333@users.noreply.github.com>
2020-07-11 20:59:44 +0800

    Add files via upload

---
+++ EggMan/Egg.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EggMan
+{
+    /// <summary>
+    /// Egg.xaml 的交互逻辑
+    /// </summary>
+    public partial class Egg : Window
+    {
+        public Egg()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Media.ImageBrush i =new System.Windows.Media.ImageBrush(new System.Windows.Media.Imaging.BitmapImage(
+                new System.Uri("egg.png", System.UriKind.RelativeOrAbsolute)));
+            i.Stretch = Stretch.UniformToFill;
+            i.TileMode = TileMode.None;
+            Background = i;
+        }
+    }
+}
---
+++ EggMan/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using MoveWindow;
+using System.Security.Cryptography;
+using System;
+using System.Windows.Shapes;
+
+namespace EggMan
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Process p = new Process();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(playSound);
+            System.Drawing.Rectangle r = System.Windows.Forms.Screen.GetWorkingArea(new System.Drawing.Point(0,0));
+            double width = r.Width;
+            double height = r.Height;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Egg egg = new Egg();
+                    egg.Left = width / 3 * j;
+                    egg.Top = height / 3 * i;
+                    egg.Show();
+                }
+                
+            }
+                
+            t.Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            System.Media.SoundPlayer s = new System.Media.SoundPlayer(System.Environment.CurrentDirectory + "\\audio\\shutdown.wav");
+            s.PlaySync();
+            s.Dispose();
+            Application.Current.Shutdown();
+        }
+        public void playSound()
+        {
+            System.Media.SoundPlayer s = new System.Media.SoundPlayer(System.Environment.CurrentDirectory + "\\audio\\startup.wav");
+            s.PlaySync();
+            s.Dispose();
+        }
+
+        
+    }
+}
